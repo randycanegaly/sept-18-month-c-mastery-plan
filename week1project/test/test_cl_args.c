585e68651d1980834a9349e725bd6cb8ec9c623f
Ubuntu <ubuntu@ip-172-31-33-107.us-east-2.compute.internal>
2025-09-10 12:23:43 +0000

    dublin 1

--- week1project/test/test_cl_args.c
+++ week1project/test/test_cl_args.c
@@ -2,13 +2,23 @@
 #include "../src/cl_args.h"
 #include <stdio.h>
 
+#define USAGE "./parse_args [-v] [-h] [-o file] [-n number] [--verbose] [--help] [--output=file] [-number=number]
+
 void setUp(void) {}
 void tearDown(void) {}
 
 void test_dummy(void) { printf("dummy test\n"); }
 
+void test_no_args(void) {
+	char *no_args[0];	
+
+	parse_args(1, no_args);	
+	TEST_ASSERT_EQUAL_STRING(USAGE, actual);
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_dummy);
+  RUN_TEST(test_no_args);
   return UNITY_END();
 }
