4c6e68cebb89f44157db43d6a7cea8c8acf65d54
randycanegaly <canegaly@gmail.com>
2025-09-14 15:13:03 -0400

    Tests and code completed for no arguments, -h and -v as sole arguments. Tests failing for -h and -v provided together on the command line.

--- week1project/src/cl_args.c
+++ week1project/src/cl_args.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "cl_args.h"
+
+char *parse_args(int num_cl_args, char *argv[]) {
+  char *result = (char *)malloc(128 * sizeof(char));
+
+  if (num_cl_args == 0) {
+    sprintf(result, NO_ARGS, num_cl_args);
+    printf("no args result: %s\n", result);
+  } else if (num_cl_args > 0 && strcmp(argv[0], "-h") == 0) {
+    sprintf(result, USAGE);
+    printf("help result: %s\n", result);
+  } else if (num_cl_args > 0 && strcmp(argv[0], "-v") == 0) {
+    sprintf(result, VERBOSE);
+    printf("verbose result: %s\n", result);
+  }
+  return result;
+}
 
 #ifdef IS_EXE
 int main(void) {
--- week1project/test/test_cl_args.c
+++ week1project/test/test_cl_args.c
@@ -2,21 +2,53 @@
 #include "../src/cl_args.h"
 #include <stdio.h>
 
-#define USAGE "./parse_args [-v] [-h] [-o file] [-n number] [--verbose] [--help] [--output=file] [-number=number]
-
 void setUp(void) {}
 void tearDown(void) {}
 
 void test_dummy(void) { printf("dummy test\n"); }
 
 void test_no_args(void) {
-	char *no_args[0];	
-	char *actual =  parse_args(1, no_args);
+  char *no_args[] = {NULL};
+  char *actual = parse_args(0, no_args);
+  char expected[128];
+  sprintf(expected, NO_ARGS, 0);
+  TEST_ASSERT_EQUAL_STRING(expected, actual);
+  // I think I need to free actual because parse_args() allocates memory
+  // for it. Where do I do that? Does execution flow get past
+  // TEST_ASSERT_EQUAL_STRING if test fails???
+}
+
+void test_help(void) {
+  char *help = "-h";
+  char *help_args[] = {help};
+  char *actual = parse_args(1, help_args);
+  char expected[128];
+  TEST_ASSERT_EQUAL_STRING(USAGE, actual);
+}
+
+void test_verbose(void) {
+  char *verbose = "-v";
+  char *verbose_args[] = {verbose};
+  char *actual = parse_args(1, verbose_args);
+  char expected[128];
+  TEST_ASSERT_EQUAL_STRING(VERBOSE, actual);
+}
+
+void test_help_and_verbose(void) {
+  char *help = "-h";
+  char *verbose = "-v";
+  char *handv[] = {help, verbose};
+  char *actual = parse_args(2, handv);
+  char expected[128];
+  TEST_ASSERT_EQUAL_STRING("busted test", actual);
 }
 
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_dummy);
   RUN_TEST(test_no_args);
+  RUN_TEST(test_help);
+  RUN_TEST(test_verbose);
+  RUN_TEST(test_help_and_verbose);
   return UNITY_END();
 }
